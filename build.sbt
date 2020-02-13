b23447978e8a7833f3c6bd1a39ed14e331ee15a4
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2020-02-13 11:47:40 +0100

    Add project files

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "tdt4305-bigdata-architecture-project"
+
+version := "0.1"
+
+scalaVersion := "2.12.10"
+val sparkVersion = "2.4.5"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % sparkVersion
---
+++ src/main/scala/task1.scala
@@ -0,0 +1,15 @@
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object task1 {
+  def main(args: Array[String]) = {
+    val conf = new SparkConf().
+      setMaster("local").
+      setAppName("task1")
+    val sc = new SparkContext(conf)
+    sc.setLogLevel("ERROR")
+
+    val helloWorldString = "Hello world"
+    print(helloWorldString)
+  }
+}
