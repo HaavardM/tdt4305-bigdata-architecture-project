2ea9db4e5e5ff862e6dd76f69fe3a7cb6272c3f5
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2020-02-20 12:22:12 +0100

    Upload task 1c

--- build.sbt
+++ build.sbt
@@ -6,3 +6,4 @@ scalaVersion := "2.12.10"
 val sparkVersion = "2.4.5"
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % sparkVersion
+libraryDependencies += "org.apache.spark" %% "spark-sql" % sparkVersion
--- src/main/scala/task1.scala
+++ src/main/scala/task1.scala
@@ -2,6 +2,7 @@
 import org.apache.spark.{SparkConf, SparkContext}
 import java.util.Base64
 import java.nio.charset.StandardCharsets
+import org.apache.spark.sql.functions.unbase64
 
 object task1 {
   def main(args: Array[String]) = {
@@ -12,15 +13,9 @@ object task1 {
     sc.setLogLevel("ERROR")
 
     //Task 0 - load data into seperate RDDs
-    val businessesRDD = sc.textFile("data/yelp_businesses.csv").mapPartitionsWithIndex((
-      (idx, iter) => if (idx == 0) iter.drop(1) else iter)
-    )
-    val topReviewersRDD = sc.textFile("data/yelp_top_reviewers_with_reviews.csv").mapPartitionsWithIndex((
-      (idx, iter) => if (idx == 0) iter.drop(1) else iter)
-    )
-    val topUsersFriendshipRDD = sc.textFile("data/yelp_top_users_friendship_graph.csv").mapPartitionsWithIndex((
-      (idx, iter) => if (idx == 0) iter.drop(1) else iter)
-    )
+    val businessesRDD = sc.textFile("data/yelp_businesses.csv")
+    val topReviewersRDD = sc.textFile("data/yelp_top_reviewers_with_reviews.csv").mapPartitionsWithIndex((index, it) => if (index == 0) it.drop(1) else it)
+    val topUsersFriendshipRDD = sc.textFile("data/yelp_top_users_friendship_graph.csv")
 
     val reviewIDIndex = 0
     val userIDIndex = 1
@@ -38,14 +33,19 @@ object task1 {
     //Task 2 - explore users
     var distinctUsers = topReviewersRDD.map(_.split("\t")(userIDIndex)).distinct()
     val reviews = topReviewersRDD.map(_.split("\t")(reviewTextIndex)).
-      map(r => new String(Base64.getDecoder().decode(r), StandardCharsets.UTF_8)).persist()
-    reviews.take(5).foreach(println)
+      map(r => new String(Base64.getMimeDecoder().decode(r), StandardCharsets.UTF_8))
     val reviewLength = reviews.map(_.length())
     val avgReviewLength = reviewLength.reduce(_+_) / numReviews
 
+    val top10Business = topReviewersRDD.map(r => (r.split("\t")(businessIDIndex), 1)).
+      reduceByKey(_+_).takeOrdered(10)(Ordering.Int.reverse.on(_._2))
+
     println("Task 2:")
-    println("a) Number of distinct users = " + distinctUsers.count())
-    println("b) Average review length    = " + avgReviewLength)
+    println("a) Number of distinct users           = " + distinctUsers.count())
+    println("b) Average review length              = " + avgReviewLength)
+    println("c) Top 10 businesses sorted by reviews: " )
+    top10Business.map(v => v._1).foreach(v => println("   - " + v))
+
     sc.stop()
   }
 }
